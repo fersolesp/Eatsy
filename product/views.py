97e22da9ad7792313b59357d7b93b7335a4d032a
Estefania <estgantri@alum.us.es>
2021-03-20 12:57:37 +0100

    feat: Vista de listado de productos
    Se ha añadido la vista, configurado las URLs y añadido el template. Issue #42

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from product.models import Producto
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -7,3 +8,19 @@ def showProduct(request, productId):
     if request.method == 'GET':
         product = get_object_or_404(Producto, pk=productId)
         return render(request, 'products/show.html', {'product': product})
+
+def listProduct(request):
+
+    products_list = Producto.objects.all()
+
+    page = request.GET.get('page')
+    paginator = Paginator(products_list,10)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    
+    return render(request, 'products/list.html', {'products': products})
