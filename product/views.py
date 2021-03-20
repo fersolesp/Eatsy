191418b778ff1c04e22362e711dc1c90ef0084fd
juaferfer11 <juaferfer11@alum.us.es>
2021-03-20 11:11:18 +0100

    feat: Limitada la vista de productos pendientes de revisión al superusuario
    He modificado el controlador de productos para que solo se muestre un producto si este está aceptado o si está pendiente pero el usuario es superusuario. En caso contrario se redirige al usuario a la página de admin y se le indica que los productos pendientes de revisión solo pueden ser vistos por el administrador.

--- product/views.py
+++ product/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from product.models import Producto
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def showProduct(request, productId):
     if request.method == 'GET':
         product = get_object_or_404(Producto, pk=productId)
-        return render(request, 'products/show.html', {'product': product})
+        if product.estado=='Pendiente' and request.user.is_superuser:
+            return render(request, 'products/show.html', {'product': product})
+        elif product.estado=='Aceptado':
+            return render(request, 'products/show.html', {'product': product})
+        else:
+            messages.error(request,'Los productos pendientes de revisión solo pueden ser vistos por el administrador.')
+            return redirect('/admin')
