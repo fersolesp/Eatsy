ffe6db625fef9effd44a0e0e4e25240434ac4c63
joscorfis <joscorfis@alum.us.es>
2021-03-24 21:42:35 +0100

    feat: Desarrollar controladores y servicios para mostrar la vista individual de reportes #45
    Se ha desarrollado la implementación necesaria para la visualización individual para el administrador de un reporte

--- product/urls.py
+++ product/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('list/<slug:estado>', views.listProductByEstado, name='listState'),
     path('create', views.createProduct, name='create'),
     path('report/<int:productId>', views.reportProduct, name='report'),
+    path('report/show/<int:reportId>', views.showReport, name='showReport'),
     path('review/<int:productId>', views.reviewProduct, name='review'),
     path('remove/<int:commentId>', views.removeComment, name='remove')
 ] 
