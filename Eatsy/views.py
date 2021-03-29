ad61cbb2c8af82d513f9668d7669bf63bc27116c
reyblacua <reyblacua@alum.us.es>
2021-03-29 23:07:55 +0200

    feat: Vista de suscripción creada
    Se ha creado la vista de suscripción con un bóton para realizar el pago close #134

--- Eatsy/views.py
+++ Eatsy/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def principalScreen(request):
      return render(request, 'products/paginaprincipal.html')
+
+def subscribe(request):
+     return render(request,'products/subscribe.html')
