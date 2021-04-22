d6de171e53e58b1113274626960b6f6528f6d197
sersanleo <sersanleo@alum.us.es>
2021-04-22 19:36:58 +0200

    feat: Controlador mostrar lista de la compra
    He creado el controlador para mostrar la lista de la compra.

--- shoppingList/models.py
+++ shoppingList/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from authentication.models import Perfil
+from django.db import models
 from product.models import Producto
+
 
 class ListaDeCompra(models.Model):
     productos = models.ManyToManyField(Producto)
