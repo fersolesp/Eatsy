50fc7ff38a417ad8a5077b1d97c8ba8e0ae2fd7f
Javier <javi77.jgc@gmail.com>
2021-04-23 10:50:01 +0200

    feat: Controladores de creación de recetas
    Se añaden los controladores para crear una receta, se adapta el form y el template. Issue #272

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,22 +1,10 @@
-from enum import Enum
-
 from django import forms
 from django.core.validators import FileExtensionValidator
-
-from .models import Receta
 from product.models import Producto
 
-
-class CreateRecipeForm(forms.ModelForm):
-    def __init__(self, productos, *args, **kwargs):
-        super(CreateRecipeForm, self).__init__(*args, **kwargs)
-        self.fields['imagen'] = forms.ImageField(label="Imagen",validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])], required=False, widget=forms.FileInput(attrs={'style':'display:None', 'accept':'image/*'}))
-        self.fields['nombre'] = forms.CharField(label='Nombre',error_messages={'required':'Este campo no puede estar vacío'}, widget=forms.TextInput(attrs={'class' : 'form-control'}) )
-        self.fields['descripcion'] = forms.CharField(label='Descripción',error_messages={'required':'Este campo no puede estar vacío'}, widget= forms.Textarea(attrs={'class' : 'form-control', 'style':'width : 100%', 'blank':'False'}))
-    
-        self.fields['productos']= forms.MultipleChoiceField(label='Selecciona un género', choices=productos, widget=forms.SelectMultiple(attrs={'class' : 'form-control', 'style':'width : 100%; height: 15em;'}))
-        
-   
-    class Meta:
-        model = Receta
-        fields = ('productos', )
+class CreateRecipeForm(forms.Form):
+    imagen = forms.ImageField(label="Imagen",validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])], required=False, widget=forms.FileInput(attrs={'style':'display:None', 'accept':'image/*'}))
+    nombre = forms.CharField(label='Nombre',error_messages={'required':'Este campo no puede estar vacío'}, widget=forms.TextInput(attrs={'class' : 'form-control'}) )
+    descripcion = forms.CharField(label='Descripción',error_messages={'required':'Este campo no puede estar vacío'}, widget= forms.Textarea(attrs={'class' : 'form-control', 'style':'width : 100%', 'blank':'False'}))
+    productos_choices = [(p.id, p.titulo) for p in Producto.objects.all().order_by('titulo')]
+    productos = forms.MultipleChoiceField(label='Selecciona un género', choices=productos_choices, widget=forms.SelectMultiple(attrs={'class' : 'form-control', 'style':'width : 100%; height: 15em;'}))
